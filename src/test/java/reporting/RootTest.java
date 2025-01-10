06819480ed7caf161afa876a7633391946ed8175
Sandip Chopkar <sandipchopkar95@gmail.com>
2025-01-10 15:07:05 +0530

    Initial commit

---
+++ src/test/java/reporting/RootTest.java
@@ -0,0 +1,8 @@
+package reporting;
+
+import com.aventstack.chaintest.plugins.ChainTestListener;
+import org.testng.annotations.Listeners;
+
+@Listeners(ChainTestListener.class)
+public class RootTest {
+}
---
+++ src/test/java/reporting/TestUtils.java
@@ -0,0 +1,24 @@
+package reporting;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestUtils extends RootTest {
+
+    @Test
+    public void firstTest(){
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void secondTest(){
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void thirdTest(){
+        Assert.assertTrue(true);
+    }
+
+
+}
