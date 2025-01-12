c74e9e3943b72d458f86100f2ed38de9d0d6216b
Sandip Chopkar <sandipchopkar95@gmail.com>
2025-01-12 12:48:09 +0530

    Updated yml with docker

--- src/test/java/reporting/RootTest.java
+++ src/test/java/reporting/RootTest.java
@@ -1,8 +1,43 @@
 package reporting;
 
 import com.aventstack.chaintest.plugins.ChainTestListener;
+import com.aventstack.chaintest.service.ChainPluginService;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
+import java.time.Duration;
 
 @Listeners(ChainTestListener.class)
 public class RootTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void attachScreenShot(final ITestResult result) {
+        try {
+            final byte[] bytes = new byte[]{};
+            final String qualifiedName = result.getMethod().getQualifiedName();
+            ChainPluginService.getInstance().embed(qualifiedName, bytes, "image/png");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @AfterMethod(dependsOnMethods = "attachScreenShot")
+    public void tearDown() {
+        driver.quit();
+    }
+
 }
+
+
--- src/test/java/reporting/TestUtils.java
+++ src/test/java/reporting/TestUtils.java
@@ -1,23 +1,40 @@
 package reporting;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestUtils extends RootTest {
 
-    @Test
-    public void firstTest(){
-        Assert.assertTrue(true);
+    @BeforeMethod(dependsOnMethods = "setUp")
+    public void setUpPage() {
+        driver.get("https://www.saucedemo.com/v1/");
     }
 
     @Test
-    public void secondTest(){
-        Assert.assertTrue(true);
+    public void verify_Login_Logo_Is_Available() {
+        WebElement logo_Login = driver.findElement(By.xpath("//div[@class='login_logo']"));
+        Assert.assertTrue(logo_Login.isDisplayed(), "Login logo not displayed");
     }
 
     @Test
-    public void thirdTest(){
-        Assert.assertTrue(true);
+    public void verify_Error_Message_Displayed_For_Empty_Credentials() {
+        driver.findElement(By.id("user-name")).sendKeys("");
+        driver.findElement(By.id("password")).sendKeys("");
+        driver.findElement(By.id("login-button")).click();
+        String errorMsg = driver.findElement(By.xpath("//h3")).getText();
+        Assert.assertTrue(errorMsg.contains("Username is required"));
+    }
+
+    @Test
+    public void verify_User_Can_Login_With_Valid_Credentials() {
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        String pageTitle = driver.findElement(By.xpath("//div[@class='product_label']")).getText();
+        Assert.assertEquals(pageTitle, "Products");
     }
 
 
